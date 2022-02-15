c9dd3987ccc91c15bf6587c64d234e55cb00a19a
sakuseimoto@gmail.com <sakuseimoto@gmail.com>
2022-02-15 09:09:38 +0900

    new

--- Ex31_hint/Program.cs
+++ Ex31_hint/Program.cs
@@ -7,7 +7,7 @@ namespace Ex31_hint
         static void Main(string[] args)
         {
             Character character1 = new Character("しむら人", 500, 2, true);
-            Character character2 = new Character("ささっき", 1000, 25, true);
+            Character character2 = new Character("ささっき", 1000, 25, false);
             Tank tank1 = new Tank("Tiger", 500000, 250, false);
             tank1.bullet = 10;
             Tank tank2 = new Tank("がんタンク", 10000, 125, false);
@@ -23,6 +23,7 @@ namespace Ex31_hint
             for(var i = 1; i < characters.Length; i++)
             {
                 characters[i].Attack(character1);
+                characters[i].Attack(character2);
             }
         }
     }
